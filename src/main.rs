ce3f053580c55e4dfbcc119d7764a57a092afa61
Alexis Lothoré <alexis.lothore@gmail.com>
2023-04-11 20:37:11 +0200

    Implement game mode with score instead of infinite loop

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,69 @@ struct Kana {
     phonetic: String,
 }
 
+#[derive(Default)]
+struct Game {
+    charset: Vec<Kana>,
+    pool: Vec<Kana>,
+    score: usize,
+}
+
+impl Game {
+    pub fn new(charset: Vec<Kana>) -> Self {
+        let mut pool = charset.clone();
+        let mut rng = thread_rng();
+        pool.shuffle(&mut rng);
+        Game {
+            charset,
+            ..Default::default()
+        }
+    }
+
+    pub fn prepare(&mut self) {
+        self.pool = self.charset.clone();
+        let mut rng = thread_rng();
+        self.pool.shuffle(&mut rng);
+        self.score = 0;
+    }
+
+    pub fn has_questions(&self) -> bool {
+        return !self.pool.is_empty();
+    }
+
+    pub fn ask(&mut self) {
+        if let Some(question) = self.pool.pop() {
+            let mut first_try = true;
+            loop {
+                let mut answer = String::new();
+                println!("{}", question.unicode);
+                io::stdin().read_line(&mut answer).unwrap();
+                answer.truncate(answer.len() - 1);
+                match answer.cmp(&question.phonetic) {
+                    std::cmp::Ordering::Equal => {
+                        println!("\u{2705}\n");
+                        break;
+                    }
+                    _ => {
+                        println!("\u{274c}\n");
+                        first_try = false;
+                    }
+                }
+            }
+            if first_try {
+                self.score += 1;
+            }
+        }
+    }
+
+    pub fn score(&self) -> usize {
+        self.score
+    }
+
+    pub fn charset_size(&self) -> usize {
+        self.charset.len()
+    }
+}
+
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long)]
@@ -34,10 +97,8 @@ fn load_characters(filepath: &str) -> Result<Vec<Kana>, Box<dyn Error>> {
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("=== Welcome to Kana training ===");
-    println!("Type \"q\" at any time to quit");
     println!("");
 
-    let mut run = true;
     let args = Args::parse();
     let dict = match args.katakana {
         true => DEFAULT_KATAKANA_DICT,
@@ -45,35 +106,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     let list = load_characters(&dict)?;
-
-    while run {
-        println!("Generating new quizz");
-        let mut pool = list.clone();
-        let mut rng = thread_rng();
-        pool.shuffle(&mut rng);
-        while !pool.is_empty() && run {
-            let question = pool.pop().unwrap();
-            loop {
-                let mut answer = String::new();
-                println!("{}", question.unicode);
-                io::stdin().read_line(&mut answer)?;
-                answer.truncate(answer.len() - 1);
-                match answer.cmp(&question.phonetic) {
-                    std::cmp::Ordering::Equal => {
-                        println!("\u{2705}\n");
-                        break;
-                    }
-                    _ => {
-                        if answer == "q" {
-                            run = false;
-                            break;
-                        } else {
-                            println!("\u{274c} ({})\n", answer)
-                        }
-                    }
-                }
-            }
-        }
+    println!("Generating new quizz");
+    let mut game = Game::new(list);
+    game.prepare();
+    while game.has_questions() {
+            game.ask();
     }
+    println!("Score: {}/{}", game.score(), game.charset_size());
     Ok(())
 }
