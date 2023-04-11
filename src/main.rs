14bca7f43d103c22789f2ad1d31b61dd1b18aba3
Alexis Lothoré <alexis.lothore@gmail.com>
2023-04-11 21:21:47 +0200

    Add infinite mode

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,10 @@ struct Kana {
 struct Game {
     charset: Vec<Kana>,
     pool: Vec<Kana>,
+    current_kana: Option<Kana>,
+    attempts: usize,
     score: usize,
-    rounds: usize
+    rounds: usize,
 }
 
 impl Game {
@@ -34,39 +36,45 @@ impl Game {
         }
     }
 
-    pub fn prepare(&mut self) {
+    pub fn reload(&mut self) {
         self.pool = self.charset.clone();
         let mut rng = thread_rng();
         self.pool.shuffle(&mut rng);
-        self.score = 0;
+        self.current_kana = None
+        // Keep score and round for infinite games
     }
 
     pub fn has_questions(&self) -> bool {
         return !self.pool.is_empty();
     }
 
-    pub fn ask(&mut self) {
-        if let Some(question) = self.pool.pop() {
-            let mut first_try = true;
-            loop {
-                self.rounds += 1;
-                let mut answer = String::new();
-                println!("{}", question.unicode);
-                io::stdin().read_line(&mut answer).unwrap();
-                answer.truncate(answer.len() - 1);
-                match answer.cmp(&question.phonetic) {
-                    std::cmp::Ordering::Equal => {
-                        println!("\u{2705}\n");
-                        break;
-                    }
-                    _ => {
-                        println!("\u{274c}\n");
-                        first_try = false;
-                    }
+    pub fn get_question(&mut self) -> Option<char> {
+        if self.current_kana.is_none() {
+            self.attempts = 0;
+            self.current_kana = self.pool.pop();
+        }
+        match &self.current_kana {
+            Some(kana) => Some(kana.unicode),
+            None => None,
+        }
+    }
+    
+    pub fn set_answer(&mut self, answer: &str) {
+        let expected = &self.current_kana.as_ref().unwrap().phonetic;
+        if self.attempts == 0 {
+            self.rounds +=1;
+        }
+        self.attempts += 1;
+        match answer.cmp(&expected) {
+            std::cmp::Ordering::Equal => {
+                println!("\u{2705}\n");
+                if self.attempts == 1 {
+                    self.score += 1;
                 }
+                self.current_kana = None
             }
-            if first_try {
-                self.score += 1;
+            _ => {
+                println!("\u{274c}\n");
             }
         }
     }
@@ -84,6 +92,9 @@ impl Game {
 struct Args {
     #[arg(short, long)]
     katakana: bool,
+
+    #[arg(short, long)]
+    infinite: bool,
 }
 
 fn load_characters(filepath: &str) -> Result<Vec<Kana>, Box<dyn Error>> {
@@ -101,6 +112,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("=== Welcome to Kana training ===");
     println!("");
 
+    let mut run = true;
     let args = Args::parse();
     let dict = match args.katakana {
         true => DEFAULT_KATAKANA_DICT,
@@ -110,9 +122,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let list = load_characters(&dict)?;
     println!("Generating new quizz");
     let mut game = Game::new(list);
-    game.prepare();
-    while game.has_questions() {
-            game.ask();
+
+    loop {
+        game.reload();
+        while game.has_questions() && run {
+            let mut answer = String::new();
+            println!("{}", game.get_question().unwrap());
+            io::stdin().read_line(&mut answer).unwrap();
+            answer.truncate(answer.len() - 1);
+            if answer == "q" {
+                run = false;
+            }
+            else {
+                game.set_answer(&answer);
+            }
+        }
+        if !args.infinite || !run {
+            break;
+        }
     }
     println!("Score: {}/{}", game.score(), game.rounds());
     Ok(())
